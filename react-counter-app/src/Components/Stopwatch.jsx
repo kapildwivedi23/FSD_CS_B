b038db2bccd98426ac9d7c4ceb997e64bc2e4e59
kapildwivedi23 <kapildwivedi23@gmail.com>
2024-12-04 19:03:58 +0530

    Added Stopwatch App

--- react-counter-app/src/Components/Stopwatch.jsx
+++ react-counter-app/src/Components/Stopwatch.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 
 const Stopwatch = () => {
-return (
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (running) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [running]);
+
+
+  const start= () => setRunning(true);
+  const stop = () => setRunning(false);
+  const reset = () => {
+    setRunning(false);
+    setTime(0);
+  };
+
+  return (
     <div>
-    <h1>stop watch App</h1>
+      <h1>Stopwatch App</h1>
+      <h2>{time} seconds</h2>
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+      <button onClick={reset}>Reset</button>
     </div>
-)
+  );
 }
 
-export default Stopwatch
+export default Stopwatch;
