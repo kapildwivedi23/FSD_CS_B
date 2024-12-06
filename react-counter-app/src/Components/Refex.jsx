3c37f709ea582bcb3eae21247261b415fd79a6f5
kapildwivedi23 <kapildwivedi23@gmail.com>
2024-12-06 10:27:42 +0530

    Use Refex

--- react-counter-app/src/Components/Refex.jsx
+++ react-counter-app/src/Components/Refex.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Refex = () => {
-    const count=useRef(0);
-    
+    const count = useRef(0);
+
+    function handleAddValue() {
+        count.current++;
+        if (count.current==5) {
+            alert("you hit button more than 5 times",count.current);
+        }
+        console.log("count.current:", count.current);
+    }
+
+    console.log("Component rendering");
+
     return (
-       <div>
-         <h1>UseRef</h1>
-       </div>
+        <div>
+            <h1>UseRef Example</h1>
+            <h2>Ref Counter: {count.current}</h2>
+            <button onClick={handleAddValue}>Add Value</button>
+        </div>
     );
-}
+};
+
 export default Refex
