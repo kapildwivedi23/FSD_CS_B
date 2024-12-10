6f8de0f065e68b9c8c7ac0231166212d4b192515
kapildwivedi23 <kapildwivedi23@gmail.com>
2024-12-10 22:49:11 +0530

    Updated Home Component

--- my-hooks-example/src/Components/Home.jsx
+++ my-hooks-example/src/Components/Home.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 const Home = () => {
   return (
     <div className='home'>
+      <h1>Home Page</h1>
       <ul>
         <li>
             <Link to="/refex">useRef Example</Link>
